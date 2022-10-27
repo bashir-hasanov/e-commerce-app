aba4c2c8025aef260294305e25767237896fa86e
bashir-hasanov <beshir.hasanov@gmail.com>
2022-10-26 23:53:31 -0400

    Customer, address, balance tables are done

---
+++ src/main/java/com/cydeo/entity/Balance.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Balance extends BaseEntity {
+
+    private BigDecimal amount;
+
+    @OneToOne
+    private Customer customer;
+
+}
